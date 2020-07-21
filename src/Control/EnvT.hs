83199abb2daf2b0b253dcb9094a4b735d9f36dfb
Yuras <shumovichy@gmail.com>
2020-07-21 11:55:49 +0300

    Extract Common.Has into a separate package (#4755)

---
+++ src/Control/EnvT.hs
@@ -0,0 +1,73 @@
+{-# LANGUAGE CPP #-}
+
+module Control.EnvT
+  ( EnvT(..)
+  , runEnvT
+  , module X
+  ) where
+
+import Control.Applicative
+import Control.Has
+import Control.Lens
+import Control.Monad.Base
+import Control.Monad.Catch
+import Control.Monad.Except
+import Control.Monad.Fail
+import Control.Monad.Reader as X
+import Control.Monad.Trans.Control
+import Control.Monad.Trans.Resource
+import Control.Monad.Zip
+#ifndef ghcjs_HOST_OS
+import Control.Lens.Missing
+import Katip
+import Katip.Monadic
+#endif
+
+
+-- | The only reason this exists is that there are a lot of instances for
+-- 'EnvT, which simply pass the implementation to the inner 'm'
+-- such as 'MonadLogger'.
+newtype EnvT r m a = EnvT { unEnvT :: ReaderT r m a}
+  deriving newtype (Functor, Applicative, Alternative, Monad
+    , MonadFix, MonadFail, Contravariant, MonadZip, MonadIO, MonadPlus)
+
+instance MonadTrans (EnvT r) where
+  lift = EnvT . lift
+
+deriving newtype instance Monad m => MonadReader r (EnvT r m)
+deriving newtype instance MonadCatch m => MonadCatch (EnvT r m)
+deriving newtype instance MonadThrow m => MonadThrow (EnvT r m)
+deriving newtype instance MonadMask m => MonadMask (EnvT r m)
+deriving newtype instance MonadError e m => MonadError e (EnvT r m)
+
+deriving newtype instance
+  ( MonadBase b m
+  , Applicative b
+  , Applicative m
+  , Monad b
+  , Monad m )
+  => MonadBase b (EnvT r m)
+
+deriving newtype instance
+  (MonadBaseControl b m, Monad m)
+  => MonadBaseControl b (EnvT r m)
+
+runEnvT :: forall r m a. EnvT r m a -> r -> m a
+runEnvT m r = flip runReaderT r . unEnvT $ m
+
+#ifndef ghcjs_HOST_OS
+instance (Has KatipContextTState r, MonadIO m)
+  => KatipContext (EnvT r m) where
+  getKatipContext = view $ part . _ltsContext
+  localKatipContext f = local (part . _ltsContext %~ f)
+  getKatipNamespace = view $ part . _ltsNamespace
+  localKatipNamespace f = local (part . _ltsNamespace %~ f)
+
+instance (Has KatipContextTState r, MonadIO m)
+  => Katip (EnvT r m) where
+  getLogEnv = view $ part . _ltsLogEnv
+  localLogEnv f = local (part . _ltsLogEnv %~ f)
+#endif
+
+instance MonadResource m => MonadResource (EnvT r m) where
+  liftResourceT = lift . liftResourceT
---
+++ src/Control/Has.hs
@@ -0,0 +1,116 @@
+
+module Control.Has
+  ( module X
+  , Has(..)
+  , HasAll
+  , MonadHas
+  , Fieldy
+  , Generic
+  )
+where
+
+import Control.Lens
+import Control.Monad.Reader as X
+import Data.Kind
+import GHC.Generics as G
+
+
+class Has x r where
+  part :: Lens r r x x
+
+instance {-# OVERLAPS #-} Has r r where
+  part = id
+
+type family HasAll (xs :: [*]) r :: Constraint where
+  HasAll '[] r = ()
+  HasAll (x ': xs) r = (Has x r, HasAll xs r)
+
+data LensList (r :: *) (es :: [*]) where
+  LLNil :: LensList r '[]
+  LLCons :: Lens r r e e -> LensList r es -> LensList r (e ': es)
+
+mapLL :: Lens s s r r -> LensList r es -> LensList s es
+mapLL _ LLNil         = LLNil
+mapLL m (LLCons l ls) = LLCons (m . l) (mapLL m ls)
+
+type family (xs :: [*]) ++ (ys :: [*]) :: [*] where
+  '[] ++ ys = ys
+  (x ': xs) ++ ys = x ': (xs ++ ys)
+
+concatLL :: LensList r es1 -> LensList r es2 -> LensList r (es1 ++ es2)
+concatLL LLNil ys         = ys
+concatLL (LLCons l ls) ys = LLCons l (concatLL ls ys)
+
+-- borrowed from Data.HSet.Get
+data N = Z | S N
+
+type family IndexOf (x :: *) (xs :: [*]) :: N where
+  IndexOf x (x ': _) = 'Z
+  IndexOf x (_ ': xs) = 'S (IndexOf x xs)
+
+class (i ~ IndexOf e es) => GetLens e es i | i es -> e where
+  getLens :: LensList r es -> Lens r r e e
+
+type Elem e es = GetLens e es (IndexOf e es)
+
+class Fieldy (r :: *) where
+  type Fields r :: [*]
+  type Fields r = GFields (Rep r)
+  lensList :: LensList r (Fields r)
+  default lensList
+    :: (Generic r, GFieldy (Rep r), Fields r ~ (GFields (Rep r)))
+    => LensList r (Fields r)
+  lensList = mapLL (iso G.from G.to) glensList
+
+instance {-# OVERLAPPABLE #-} (Fieldy r, Elem e (Fields r)) => Has e r where
+  part = getLens lensList
+
+class (MonadReader r m, HasAll es r)
+  => MonadHas (es :: [*]) (r :: *) (m :: * -> *) | m -> r
+instance (MonadReader r m, HasAll es r) => MonadHas es r m
+
+instance GetLens e (e ': es) 'Z where
+  getLens (LLCons l _) = l
+
+instance (GetLens e' es n, 'S n ~ IndexOf e' (e ': es))
+  => GetLens e' (e ': es) ('S n) where
+  getLens (LLCons _ ls) = getLens ls
+
+class GFieldy (r :: * -> *) where
+  type GFields r :: [*]
+  glensList :: LensList (r x) (GFields r)
+
+instance GFieldyProduct r => GFieldy (D1 d (C1 c r)) where
+  type GFields (D1 d (C1 c r)) = GFieldsProduct r
+  glensList = mapLL (iso unM1 M1 . iso unM1 M1) glensListProduct
+
+class GFieldyProduct (r :: * -> *) where
+  type GFieldsProduct r :: [*]
+  glensListProduct :: LensList (r x) (GFieldsProduct r)
+
+instance (GFieldyProduct r, GFieldyProduct s) => GFieldyProduct (r :*: s) where
+  type GFieldsProduct (r :*: s) = GFieldsProduct r ++ GFieldsProduct s
+  glensListProduct =
+    concatLL
+      (mapLL (lens lget lset) glensListProduct)
+      (mapLL (lens rget rset) glensListProduct)
+    where
+      lget (l :*: _) = l
+      lset (_ :*: r) l = l :*: r
+      rget (_ :*: r) = r
+      rset (l :*: _) r = l :*: r
+
+instance GFieldyProduct (S1 s (K1 k r)) where
+  type GFieldsProduct (S1 s (K1 k r)) = '[r]
+  glensListProduct = LLCons (iso unM1 M1 . iso unK1 K1) LLNil
+
+type family AllElem (xs :: [*]) (es :: [*]) :: Constraint where
+  AllElem '[] _ = ()
+  AllElem (x ': xs) es = (Elem x es, AllElem xs es)
+
+instance Fieldy (a, b)
+instance Fieldy (a, b, c)
+instance Fieldy (a, b, c, d)
+instance Fieldy (a, b, c, d, e)
+instance Fieldy (a, b, c, d, e, f)
+instance Fieldy (a, b, c, d, e, f, g)
